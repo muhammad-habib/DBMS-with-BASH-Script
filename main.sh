3e9c93776e5d4b6af6a88e7d2656c7e1da06469a
terminator <muhammad.habib.dev@gmail.com>
2017-01-12 07:42:24 +0200

    push main functions with error in array index when droping database

--- main.sh
+++ main.sh
@@ -2,9 +2,11 @@
 #create user menu function
 
 readonly DBPATH="databases";
-declare –a DBARR;
 
 
+
+##########################################################################################
+# create Database  is called in userInterface function
 function createDB {
 	read -p "Enter Database Name : " dbName;
 	if [[ ! -d $DBPATH/$dbName ]];
@@ -14,36 +16,105 @@ function createDB {
 				else	
 					echo "Error Done While Creating the Database" ;
 				fi
-
 	else	
-		echo "This Database is Exists"
+		echo "This Database is Exists";
+	fi
+
+}
+########################################################################################
+# List Databases in databases Dir  is called in userInterface function
+function listDB {
+	declare -a DBARR ;
+	i=1;
+	for DB in `ls $DBPATH`
+	do
+		DBARR[$i]=$DB;
+		let i=i+1;
+	done
+
+	if [[ ${#DBARR[@]} -eq 0 ]]; 
+		then
+			echo "There Is No Database To Be Listed";
+			userInterface;
+	fi
+
+	echo "This Is List With Available Databases : ";
+	i=1;
+	for DB in `ls $DBPATH`
+	do
+		DBARR[$i]=$DB;
+		echo $i") "$DB;
+		let i=i+1;
+	done
+	echo "${DBARR[@]}";
+
+	if [[ ! "$1" ]]; then
+		return 0;
+	fi
+
+	if [[ "$1"=="show" ]]; then
+		userInterface;
+	fi
+}
+#######################################################################################
+# Drop Database From Databases list
+function dropDB {
+	read -p "Choose Database You Want To Drop It From The Above Databases List : " choise ;
+	containsElement ${DBARR[$choise]} "${DBARR[@]}";
+	if [[  "$?" == "1" ]]; then
+		read -p "Are You Sure Droping ${DBARR[$choise]} Database [Y/N] " response;
+		case $response in 
+			[yY][eE][sS]|[yY]) 
+	        	rm -r $DBPATH/${DBARR[$choise]};
+	        	echo "${DBARR[@]}";
+	    	;;
+
+	    	*)
+				printHash;
+				userInterface;
+			;;
+		esac	
+	else
+		printHash;
+		echo "out of range";
+		listDB;
+		dropDB;
 
 	fi
 
 }
 
-function listDB {
-	#read -p "Choose Your Database From The Below List : " choise ;
-	local i=0;
-	for DB in `ls $DBPATH`
-	do
-	   DBARR[$i]=$DB;
-	   let i=i+1;
-	done
-	
-
+#######################################################################################
+# check if array contain an element return 1 if found return 0 if not
+containsElement () {
+    local e
+    for e in "${@:2}"
+    do 
+        if [[ "$e" == "$1" ]]
+            then 
+                return 1;
+        fi 
+    done
+  
+    return 0
+}
+#######################################################################################
+# print #
+function printHash {
+	echo "############################################################################";
 }
 
 
-
-
-
-
-function  userInterface {
-
+##########################################################################################
+# Just Welcome Message
+function welcome {
 	echo -e "\n \n \nWelcome to Habib DBMS\n \n \n";
+}
 
-	options=("create New Database" "Choose Database" "Quit");
+########################################################################################
+function  userInterface {
+	printHash;
+	options=("create New Database" "Choose Database" "Show Databases" "Drop Databas" "Quit");
 
 	PS3="Enter Your Choice : " ;
 
@@ -51,11 +122,26 @@ function  userInterface {
 	do
 		case $opt in
 			"create New Database")
+				printHash
 				createDB;
+				userInterface;
 				break ;
 				;;
 			"Choose Database")
+				printHash;
 				listDB;
+				break ;
+				;;
+			"Show Databases")
+				printHash;
+				listDB "show";
+				break ;
+				;;
+			"Drop Databas")
+				printHash;
+				listDB;
+				dropDB;
+				userInterface
 				break ;
 				;;
 			"Quit")
@@ -64,6 +150,7 @@ function  userInterface {
 		esac
 	done
 };
-
+printHash;
+welcome;
 userInterface;
 
--- test.sh
+++ test.sh
@@ -1,26 +1,10 @@
-PS3='Please enter your choice: '
-options=("Option 1" "Option 2" "Option 3" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Option 3")
-            echo "you chose choice 1"
-            ;;
-        "Option 2")
-            echo "you chose choice 2"
-            ;;
-        "Option 1")
-            echo "you chose choice 3"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
-done
+#!/bin/bash
 
-        if [[ $? -eq 0 ]]; then
-            echo $dbName" Database Created Successfully" ;
-        else    
-            echo "Error Done While Creating the Database" ;
-        fi  
+my_array=(red orange green)
+value='green'
+
+for i in "${!my_array[@]}"; do
+   if [[ "${my_array[$i]}" = "${value}" ]]; then
+       echo "${i}";
+   fi
+done
